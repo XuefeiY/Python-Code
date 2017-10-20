4bc46ec4833d7bf7db4cf386ad419ec3f5ff1f7c
Xuefei Yang <yangxuefei@hotmail.com>
2017-10-20 17:02:56 -0400

    Add files via upload

---
+++ words frequency.py
@@ -0,0 +1,71 @@
+#==============================================================================
+# # Word frequency
+#==============================================================================
+import re
+import string
+frequency = {}
+document_text = "Tomorrow is Friday. Look forward to the Labor Day weekend."
+text_string = document_text.lower()
+match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
+ 
+for word in match_pattern:
+    count = frequency.get(word,0)
+    frequency[word] = count + 1
+     
+frequency_list = frequency.keys()
+ 
+for words in frequency_list:
+    print (words, frequency[words])
+    
+    
+
+#==============================================================================
+# # Finding Patterns in Text
+#==============================================================================
+patterns = [ 'this', 'that' ]
+text = 'Does this text match the pattern?'
+
+for pattern in patterns:
+    print ('Looking for "%s" in "%s" ->' % (pattern, text),)
+    if re.search(pattern,  text):
+        print ('found a match!')
+    else:
+        print ('no match')
+    
+#==============================================================================
+# # Sentiment Analysis
+#==============================================================================
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import names
+ 
+def word_feats(words):
+    return dict([(word, True) for word in words])
+ 
+positive_vocab = [ 'awesome', 'outstanding', 'fantastic', 'terrific', 'good', 'nice', 'great', ':)' ]
+negative_vocab = [ 'bad', 'terrible','useless', 'hate', ':(' ]
+neutral_vocab = [ 'movie','the','sound','was','is','actors','did','know','words','not' ]
+ 
+positive_features = [(word_feats(pos), 'pos') for pos in positive_vocab]
+negative_features = [(word_feats(neg), 'neg') for neg in negative_vocab]
+neutral_features = [(word_feats(neu), 'neu') for neu in neutral_vocab]
+ 
+train_set = negative_features + positive_features + neutral_features
+ 
+classifier = NaiveBayesClassifier.train(train_set) 
+ 
+# Predict
+neg = 0
+pos = 0
+sentence = "Awesome movie, I liked it"
+sentence = sentence.lower()
+words = sentence.split(' ')
+for word in words:
+    classResult = classifier.classify( word_feats(word))
+    if classResult == 'neg':
+        neg = neg + 1
+    if classResult == 'pos':
+        pos = pos + 1
+ 
+print('Positive: ' + str(float(pos)/len(words)))
+print('Negative: ' + str(float(neg)/len(words)))
